37ce1d7b71aef8f3bed2d63e31b5e696608617ee
Neemias Chagas de Souza Junior <neemias.junior@ftd.com.br>
2021-11-24 21:24:32 -0300

    Remove the now ignored sql

--- database-promobitdesafio.sql
+++
@@ -1,6 +0,0 @@
-create database promobitdesafio;
-
-CREATE USER 'userdesafioapp'@'%' IDENTIFIED BY '@#promobit';
-ALTER USER 'userdesafioapp'@'%' IDENTIFIED BY 'promobit';
-
-GRANT ALL PRIVILEGES ON *.* TO 'userdesafioapp'@'%' WITH GRANT OPTION;
